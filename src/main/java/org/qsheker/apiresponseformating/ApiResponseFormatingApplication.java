38134153f90dafbb049b363b2f219b0a33f8e3ca
Aldiyar <aldikzhaks@gmail.com>
2025-11-08 16:56:14 +0500

    first commit

---
+++ src/main/java/org/qsheker/apiresponseformating/ApiResponseFormatingApplication.java
@@ -0,0 +1,13 @@
+package org.qsheker.apiresponseformating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiResponseFormatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiResponseFormatingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/qsheker/apiresponseformating/ApiResponseFormatingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.qsheker.apiresponseformating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiResponseFormatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
